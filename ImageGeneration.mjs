c89a969cc9edfa828e68fd2e673e9cf853179c39
J. Rosenbaum <18021993+minxdragon@users.noreply.github.com>
2020-09-07 15:38:03 +1000

    updated rotation info

--- ImageGeneration.mjs
+++ ImageGeneration.mjs
@@ -23,7 +23,7 @@
     var FR= new FileReader();
     
     FR.addEventListener("load", function(e) {
-      document.getElementById("img").src       = e.target.result;
+      document.getElementById("imgUpload").src       = e.target.result;
       //document.getElementById("b64").innerHTML = e.target.result;
     }); 
     
@@ -33,7 +33,7 @@
 }
 document.getElementById("inp").addEventListener("change", readFile);
 
-document.getElementById('img').onchange = function (e) {
+document.getElementById('imgUpload').onchange = function (e) {
     var loadingImage = loadImage(
         e.target.files[0],
         function (img) {
@@ -41,10 +41,80 @@ document.getElementById('img').onchange = function (e) {
         },
         {orientation: 1}
     );
-    if (!loadingImage) {
+    //if (!loadingImage) {
         // Alternative code ...
-    }
+    //}
 };
+
+var originalImage = document.getElementById("imgUpload"),
+	resetImage = document.getElementById("img");
+
+function readFile() {
+  if (this.files && this.files[0]) {
+    var FR= new FileReader();
+    FR.addEventListener("load", function(e) {
+    	// Here is the code that gets called when the JS first gets its hands on the image.
+      const rawImageData = e.target.result;
+      
+      // Set the "original" view:
+      document.getElementById("imgUpload").src = rawImageData;
+
+			// Should be able to do rotation right here.
+			resetOrientation(rawImageData, 1, function(resetBase64Image) {
+        console.log( resetBase64Image );
+        resetImage.src = resetBase64Image;
+        console.log("rotated image")
+      });
+    } );
+
+    FR.readAsDataURL( this.files[0] );
+  }
+}
+
+// call file loader when the user picks a file to upload.
+document.getElementById("inp").addEventListener("change", readFile);
+
+//rotate orientation
+function resetOrientation(srcBase64, srcOrientation, callback) {
+	var img = new Image();	
+
+	img.onload = function() {
+  	var width = img.width,
+    		height = img.height,
+        canvas = document.createElement('canvas'),
+	  		ctx = canvas.getContext("2d");
+		
+    // set proper canvas dimensions before transform & export
+		if (4 < srcOrientation && srcOrientation < 9) {
+    	canvas.width = height;
+      canvas.height = width;
+    } else {
+    	canvas.width = width;
+      canvas.height = height;
+    }
+	
+  	// transform context before drawing image
+	switch (srcOrientation) {
+      case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+      case 3: ctx.transform(-1, 0, 0, -1, width, height ); break;
+      case 4: ctx.transform(1, 0, 0, -1, 0, height ); break;
+      case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+      case 6: ctx.transform(0, 1, -1, 0, height , 0); break;
+      case 7: ctx.transform(0, -1, -1, 0, height , width); break;
+      case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+     default: break;
+    }
+
+		// draw image
+    ctx.drawImage(img, 0, 0);
+
+    // export base64
+    callback(canvas.toDataURL('image/jpeg'));
+    //canvas.toBlob('image/jpeg', 0.15);
+  };
+
+  img.src = srcBase64;
+}
     
 // start deeplab model
     function deepLabModelSelfie(){
